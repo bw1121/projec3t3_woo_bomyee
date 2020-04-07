1925bd766cf072aeff81f113ae7a66e4ee577c69
Bomyee Woo <bomyeewoo@Bomyees-MacBook-Pro.local>
2020-04-07 00:17:41 -0400

    fixed js naming and script tags for about

--- js/home.js
+++ js/home.js
@@ -1,9 +1,4 @@
 //change quote in speech bubble on hover - homepage
-// document.getElementsByClassName("speech-bubble").addEventListener("mouseenter",
-//   function() {
-//     document.getElementById("quote").innerHTML = "I&rsquo;m Responsible For My Own Happiness? I Can’t Even Be Responsible For My Own Breakfast!"
-//   }, false
-// );
 
 document.getElementById("bubble").addEventListener("mouseenter",
   function () {
