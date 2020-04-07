a77a8e45251dc634a67606ceb81f7acdc0b2fad6
Bomyee Woo <bomyeewoo@Bomyees-MacBook-Pro.local>
2020-04-07 00:15:40 -0400

    added js and animation to homepage

---
+++ js/home.js
@@ -0,0 +1,12 @@
+//change quote in speech bubble on hover - homepage
+// document.getElementsByClassName("speech-bubble").addEventListener("mouseenter",
+//   function() {
+//     document.getElementById("quote").innerHTML = "I&rsquo;m Responsible For My Own Happiness? I Can’t Even Be Responsible For My Own Breakfast!"
+//   }, false
+// );
+
+document.getElementById("bubble").addEventListener("mouseenter",
+  function () {
+    document.getElementById("quote").innerHTML = "You Turn Yourself Around. That’s What It’s All About.";
+  }, false
+);
